4b6a1a650f317a2c34f1f5b0ea9c1e4d524d868d
Scott Tenefrancia <54250471+tenefra@users.noreply.github.com>
2020-08-28 14:27:40 -0500

    Update SC_AddMultipleItems.java

--- Selenium Test Suites/YetiAutomation/src/shoppingCart/SC_AddMultipleItems.java
+++ Selenium Test Suites/YetiAutomation/src/shoppingCart/SC_AddMultipleItems.java
@@ -16,15 +16,11 @@ public class SC_AddMultipleItems {
 		System.setProperty("webdriver.chrome.driver", "C:\\Testing\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 
-		String[] itemsNeeded = { "RAMBLER 20 OZ TUMBLER WITH MAGSLIDER LID", "RAMBLER 30 OZ TUMBLER WITH MAGSLIDER LID",
-				"RAMBLER 12 OZ COLSTER CAN INSULATOR", "RAMBLER 14 OZ MUG WITH STANDARD LID",
-				"RAMBLER HALF GALLON JUG" };
+		String[] itemsNeeded = {"RAMBLER 20 OZ TUMBLER WITH MAGSLIDER LID","RAMBLER 30 OZ TUMBLER WITH MAGSLIDER LID","RAMBLER 12 OZ COLSTER CAN INSULATOR","RAMBLER 14 OZ MUG WITH STANDARD LID","RAMBLER HALF GALLON JUG" };
 		int j = 0;
 
 		driver.get("https://www.yeti.com");
-		driver.findElement(By.xpath(
-				"//body/div[@id='wrapper']/div[@class='background-image-hp']/header[@class='header-main']/div[@class='header-row header-row-secondary']/nav[@class='nav-main']/ul[@class='yeti-nav-level-1 show-for-medium-custom']/li/div[@class='yeti-nav-container']/div[@id='primary-nav-yeti']/ul[@class='primary-nav-list-yeti']/li[2]"))
-				.click();
+		driver.findElement(By.xpath("//body/div[@id='wrapper']/div[@class='background-image-hp']/header[@class='header-main']/div[@class='header-row header-row-secondary']/nav[@class='nav-main']/ul[@class='yeti-nav-level-1 show-for-medium-custom']/li/div[@class='yeti-nav-container']/div[@id='primary-nav-yeti']/ul[@class='primary-nav-list-yeti']/li[2]")).click();
 		Thread.sleep(3000);
 		List<WebElement> products = driver.findElements(By.cssSelector("h3[class='product-name']"));
 
